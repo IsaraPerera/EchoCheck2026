0e856364bc3528ae2c22a7f7f529ffe2df73ff74
Admin <isaraperera2005@gmail.com>
2026-08-29 20:45:44 +0530

    create user and get selected user are defined

--- src/main/java/com/example/ecocheck2026/EcoCheck2026Application.java
+++ src/main/java/com/example/ecocheck2026/EcoCheck2026Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EcoCheck2026Application {
 
-    public static void main(String[] args) {
+     static void main(String[] args) {
         SpringApplication.run(EcoCheck2026Application.class, args);
     }
 
--- src/main/java/com/example/ecocheck2026/controller/userController.java
+++ src/main/java/com/example/ecocheck2026/controller/userController.java
@@ -1,17 +1,26 @@
 package com.example.ecocheck2026.controller;
 
 import com.example.ecocheck2026.dto.UserDTO;
+import com.example.ecocheck2026.dto.enums.Role;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("v1/api/users")
+
+@RequestMapping("api/v1/users")
 @RestController
 public class userController {
-    @PostMapping
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void>createUser(@RequestBody UserDTO userDTO){
+        System.out.println("create user:" + userDTO.toString());
+        return  new ResponseEntity<>(HttpStatus.CREATED);
+
+    }
+    @GetMapping(name =("userId"),produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<UserDTO>getSelectedUser(@PathVariable String userId){
+        System.out.println("selected user:" + userId);
+        return  new ResponseEntity<>(new UserDTO("U0001","Isara","Perera","isaraperera2005@gmail.com","Issa",Role.ADMIN),HttpStatus.OK);
 
     }
 }
