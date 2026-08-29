7bde0511a531c0d0fe7a9bd48a81654069fe1d1d
Admin <isaraperera2005@gmail.com>
2026-08-29 23:31:00 +0530

    Get ,Getall and Update users

--- src/main/java/com/example/ecocheck2026/controller/userController.java
+++ src/main/java/com/example/ecocheck2026/controller/userController.java
@@ -7,8 +7,11 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 
-@RequestMapping("api/v1/users")
+
+@RequestMapping("/api/v1/users")
 @RestController
 public class userController {
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
@@ -17,10 +20,39 @@ public class userController {
         return  new ResponseEntity<>(HttpStatus.CREATED);
 
     }
-    @GetMapping(name =("userId"),produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value ={"/{userId}"},produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<UserDTO>getSelectedUser(@PathVariable String userId){
         System.out.println("selected user:" + userId);
         return  new ResponseEntity<>(new UserDTO("U0001","Isara","Perera","isaraperera2005@gmail.com","Issa",Role.ADMIN),HttpStatus.OK);
+
+    }
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<UserDTO>>getUsers(){
+        List<UserDTO> userList= List.of(
+                new UserDTO(
+                        "U0001",
+                        "Isara",
+                        "Perera",
+                        "isaraperera2005@gmail.com",
+                        "Issa",
+                        Role.ADMIN
+                ),
+                new UserDTO(
+                        "U0002",
+                        "Tusheni",
+                        "Perera",
+                        "tushi@gmail.com",
+                        "tush",
+                        Role.ADMIN
+                ));
+                return  new ResponseEntity<>(userList,HttpStatus.OK);
+
+
+    }
+    @PatchMapping(value ={"/{userId}"},consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void>updateUser(@PathVariable String userId,@RequestBody UserDTO userDTO){
+        System.out.println("To be updated the userid : " +userId+ "as:" + userDTO.toString());
+        return  new ResponseEntity<>(HttpStatus.NO_CONTENT);
 
     }
 }
