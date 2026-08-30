8a9d712d74aec98572f7f10cf17f3b940c9f4ceb
Admin <isaraperera2005@gmail.com>
2026-08-30 16:44:04 +0530

    user dependency injection

--- src/main/java/com/example/ecocheck2026/controller/userController.java
+++ src/main/java/com/example/ecocheck2026/controller/userController.java
@@ -1,7 +1,9 @@
 package com.example.ecocheck2026.controller;
 
 import com.example.ecocheck2026.dto.UserDTO;
+import com.example.ecocheck2026.service.UserService;
 import com.example.ecocheck2026.service.impl.UserServiceIMPL;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -12,28 +14,27 @@ import java.util.List;
 
 @RequestMapping("/api/v1/users")
 @RestController
+@RequiredArgsConstructor
 public class userController {
+    private final UserService userService;
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void>createUser(@RequestBody UserDTO userDTO){
-        var userServiceImpl = new UserServiceIMPL();
-        userServiceImpl.saveUser(userDTO);
+        userService.saveUser(userDTO);
         return  new ResponseEntity<>(HttpStatus.CREATED);
 
     }
     @GetMapping(value ={"/{userId}"},produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<UserDTO>getSelectedUser(@PathVariable String userId){
         System.out.println("selected user:" + userId);
-        var userServiceIMPL = new UserServiceIMPL();
-        return  new ResponseEntity<>(userServiceIMPL.getSelectedUser(userId),HttpStatus.OK);
+        return  new ResponseEntity<>(userService.getSelectedUser(userId),HttpStatus.OK);
 
     }
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<UserDTO>>getAllUsers(){
-        return  new ResponseEntity<>(new UserServiceIMPL().getAllUsers(),HttpStatus.OK);
+        return  new ResponseEntity<>(userService.getAllUsers(),HttpStatus.OK);
     }
     @PatchMapping(value ={"/{userId}"},consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void>updateUser(@PathVariable String userId,@RequestBody UserDTO userDTO){
-        new UserServiceIMPL().updateUser(userId,userDTO);
         return  new ResponseEntity<>(HttpStatus.NO_CONTENT);
 
     }
