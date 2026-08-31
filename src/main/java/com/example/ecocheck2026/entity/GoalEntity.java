50ef3c0bb081169c3bf35fd546f84921d9c9d330
Admin <isaraperera2005@gmail.com>
2026-08-31 22:24:07 +0530

    create other entities - partial

--- src/main/java/com/example/ecocheck2026/entity/GoalEntity.java
+++ src/main/java/com/example/ecocheck2026/entity/GoalEntity.java
@@ -1,14 +1,22 @@
 package com.example.ecocheck2026.entity;
 
 import com.example.ecocheck2026.dto.enums.GoalStatus;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Entity
+@Table(name="goals")
+@Builder
 public class GoalEntity {
+    @Id
     private String id;
     private String title;
     private String targetReduction;
--- src/main/java/com/example/ecocheck2026/entity/UserActionEntity.java
+++ src/main/java/com/example/ecocheck2026/entity/UserActionEntity.java
@@ -1,13 +1,21 @@
 package com.example.ecocheck2026.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Entity
+@Table(name = "user-actions")
+@Builder
 public class UserActionEntity {
+    @Id
     private String userActionId;
     private String quantity;
     private String totalReduction;
