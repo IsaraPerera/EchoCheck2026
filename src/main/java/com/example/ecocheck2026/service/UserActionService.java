95353ab682e4ee8f592eff9c95943fb96f280593
Admin <isaraperera2005@gmail.com>
2026-09-05 00:24:57 +0530

    in the middle of creating user actionSERVICEIMPL

--- src/main/java/com/example/ecocheck2026/service/UserActionService.java
+++ src/main/java/com/example/ecocheck2026/service/UserActionService.java
@@ -1,13 +1,14 @@
 package com.example.ecocheck2026.service;
 
 import com.example.ecocheck2026.dto.UserActionDTO;
+import org.springframework.data.crossstore.ChangeSetPersister;
 
 import java.util.List;
 
 
 public interface UserActionService {
     void saveUserAction(UserActionDTO userAction);
-    UserActionDTO getSelectedUserAction(String userActionId);
+    UserActionDTO getSelectedUserAction(String userActionId) throws ChangeSetPersister.NotFoundException;
     List<UserActionDTO> getAllUserActions();
     void updateUserAction(String userActionId, UserActionDTO userAction);
     void deleteUserAction(String userActionId);
--- src/main/java/com/example/ecocheck2026/service/impl/UserActionServiceIMPL.java
+++ src/main/java/com/example/ecocheck2026/service/impl/UserActionServiceIMPL.java
@@ -1,48 +1,63 @@
 package com.example.ecocheck2026.service.impl;
 
+import com.example.ecocheck2026.dao.UserActionDAO;
+import com.example.ecocheck2026.dao.UserDAO;
 import com.example.ecocheck2026.dto.UserActionDTO;
+import com.example.ecocheck2026.entity.ClimateActionEntity;
+import com.example.ecocheck2026.entity.UserActionEntity;
+import com.example.ecocheck2026.entity.UserEntity;
+import com.example.ecocheck2026.exceptions.DataNotFoundException;
 import com.example.ecocheck2026.service.UserActionService;
+import com.example.ecocheck2026.util.Conversion;
+import com.example.ecocheck2026.util.IDGenerate;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
+@RequiredArgsConstructor
+@Transactional
 public class UserActionServiceIMPL implements UserActionService {
+
+    private final Conversion conversion;
+    private final UserActionDAO userActionDAO;
+    private final UserDAO userDAO;
+
     @Override
     public void saveUserAction(UserActionDTO userAction) {
-        System.out.println("create user action :" + userAction.toString());
+        userAction.setActionId(IDGenerate.userActionId());
+        UserActionEntity userActionEntity = conversion.toUserActionEntity(userAction);
+        userActionDAO.save(userActionEntity);
     }
 
     @Override
-    public UserActionDTO getSelectedUserAction(String userActionId) {
-        System.out.println("selected user action :" + userActionId);
-        return new UserActionDTO("UA-901","3","37.5 kgCO2e","2026-08-15","U0001", "ACT-101");
+    public UserActionDTO getSelectedUserAction(String userActionId) throws ChangeSetPersister.NotFoundException {
+        UserActionEntity userActionEntity = userActionDAO.findById(userActionId)
+                .orElseThrow(() -> new DataNotFoundException("User Action Not Found"));
+        return conversion.toUserActionDTO(userActionEntity);
     }
 
     @Override
     public List<UserActionDTO> getAllUserActions() {
-        List<UserActionDTO> userActionList= List.of(
-                new UserActionDTO(
-                        "UA-901",
-                        "3",
-                        "37.5 kgCO2e",
-                        "2026-08-15",
-                        "U0001",
-                        "ACT-101"
-                ),
-                new UserActionDTO(
-                        "UA-902",
-                        "1",
-                        "4.2 kgCO2e",
-                        "2026-08-28",
-                        "U0002",
-                        "ACT-102"
-                ));
-        return userActionList;
+        return conversion.toUserActionDTOList(userActionDAO.findAll());
     }
 
     @Override
     public void updateUserAction(String userActionId, UserActionDTO userAction) {
-        System.out.println("To be updated the user action id : " +userActionId+ "as:" + userAction.toString());
+        UserEntity foundUserEntity = userDAO.findById(userActionDTO.getUserId())
+                .orElseThrow(() -> new DataNotFoundException("User Not Found"));
+        ClimateActionEntity foundClimateActionEntity = climateActionDAO.findById(userActionDTO.getClimateActionId())
+                .orElseThrow(() -> new DataNotFoundException("Climate Action Not Found"));
+        UserActionEntity foundUserActionEntity = userActionDAO.findById(userActionId)
+                .orElseThrow(() -> new DataNotFoundException("User Action Not Found"));
+
+        foundUserActionEntity.setClimateAction(foundClimateActionEntity);
+        foundUserActionEntity.setUser(foundUserEntity);
+        foundUserActionEntity.setQuantity(userActionDTO.getQuantity());
+        foundUserActionEntity.setTotalReduction(userActionDTO.getTotalReduction());
+
     }
 
     @Override
